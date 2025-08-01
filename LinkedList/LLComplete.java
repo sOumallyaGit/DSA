139b06cd2c309bbb1de7cbec46535ec92c53f92e
sOumallyaGit <soumallya2112@gmail.com>
2025-08-02 00:52:48 +0530

    Refactor insert method in SinglyLL for improved readability and functionality

--- LinkedList/LLComplete.java
+++ LinkedList/LLComplete.java
@@ -22,7 +22,7 @@ class SinglyLL{
         }
         size++;
     }
-    
+
     void insertAtHead(int val){
         LLNode temp = new LLNode(val);
         if(head==null){
@@ -48,17 +48,41 @@ class SinglyLL{
         System.out.println("The Length of your LinkedList is :"+size);
     }
 
-    
+    void insert(int index,int value){
+        if(index==0){
+            insertAtHead(value);
+            return;
+        }
+        if(index==size){
+            insertAtTail(value);
+            return;
+        }
+        if(index<0 || index>size){
+            System.out.println("Invalid Index");
+            return ;
+        }
+        LLNode temp = new LLNode(value);
+        LLNode x = head;
+        for(int i = 1 ; i<index ; i++){
+            x = x.next;
+        }
+        temp.next=x.next;
+        x.next=temp;
+        size++;
+    }
 }
 
 public class LLComplete {
     public static void main(String[] args) {
         SinglyLL LinkedList1 = new SinglyLL();
+        LinkedList1.insertAtTail(3);
+        LinkedList1.insertAtTail(4);
         LinkedList1.insertAtTail(5);
-        LinkedList1.insertAtTail(5);
-        LinkedList1.insertAtTail(5);
-        LinkedList1.insertAtHead(10);
+        LinkedList1.insertAtHead(1);
         LinkedList1.Display();
         LinkedList1.Length();
+
+        LinkedList1.insert(1,2);
+        LinkedList1.Display();
     }
 }
